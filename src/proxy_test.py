a09954e67850ebee12516c66cf7550a0ee222e91
shuhana <shuhana.azmin@gmail.com>
2019-03-11 10:59:48 +0600

    reverse_proxy

---
+++ src/proxy_test.py
@@ -0,0 +1,19 @@
+# Copyright (c) Twisted Matrix Laboratories.
+# See LICENSE for details.
+
+"""
+This example demonstrates how to run a reverse proxy.
+
+Run this example with:
+    $ python reverse-proxy.py
+
+Then visit http://localhost:8080/ in your web browser.
+"""
+
+from twisted.internet import reactor
+from twisted.web import  server
+import reverse_proxy
+
+site = server.Site(reverse_proxy.ReverseProxyResource('www.yahoo.com', 80, b''))
+reactor.listenTCP(8080, site)
+reactor.run()
